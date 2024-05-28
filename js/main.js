b76a14f2a85663f43acafb5b5bf712886554548d
Ethan Cornwill <ethancornwill@outlook.com>
2024-05-28 22:49:00 +1000

    major style change and improvement

--- js/main.js
+++ js/main.js
@@ -2,14 +2,7 @@ let section = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('nav a');
 
 window.onscroll = function() {
-    var navbar = document.getElementById("navbar");
-    if (document.documentElement.scrollTop > 60) {
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
-    }
-
-    section.forEach(sec => {
+        section.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
